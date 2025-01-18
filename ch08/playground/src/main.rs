8504345762ff7efee7893451d6c1453ca19044c3
Daniel Ceballos <daniel.ceballos.arvizu@gmail.com>
2025-01-18 16:19:39 -0600

    key-value

--- ch08/playground/src/main.rs
+++ ch08/playground/src/main.rs
@@ -4,51 +4,139 @@ use tokio::sync::{
     oneshot,
 };
 
-struct Message {
-    value: u64,
+static ROUTER_SENDER: OnceLock<Sender<RoutingMessage>> = OnceLock::new();
+
+use std::sync::OnceLock;
+
+struct SetKeyValueMessage {
+    key: String,
+    value: Vec<u8>,
+    response: oneshot::Sender<()>,
 }
 
-async fn basic_actor(mut rx: Receiver<Message>) {
-    let mut state = 0;
-
-    while let Some(msg) = rx.recv().await {
-        state += msg.value;
-        println!("Received: {}", msg.value);
-        println!("State: {}", state);
-    }
+struct GetKeyValueMessage {
+    key: String,
+    response: oneshot::Sender<Option<Vec<u8>>>,
 }
 
-struct RespMessage {
-    value: i32,
-    responder: oneshot::Sender<i32>,
+struct DeleteKeyValueMessage {
+    key: String,
+    response: oneshot::Sender<()>,
 }
 
-async fn resp_actor(mut rx: Receiver<RespMessage>) {
-    let mut state = 0;
+enum KeyValueMessage {
+    Get(GetKeyValueMessage),
+    Delete(DeleteKeyValueMessage),
+    Set(SetKeyValueMessage),
+}
 
-    while let Some(msg) = rx.recv().await {
-        state += msg.value;
+enum RoutingMessage {
+    KeyValue(KeyValueMessage),
+}
 
-        if msg.responder.send(state).is_err() {
-            eprintln!("Failed to send response");
+async fn key_value_actor(mut receiver: Receiver<KeyValueMessage>) {
+    let mut map: std::collections::HashMap<String, Vec<u8>> = std::collections::HashMap::new();
+
+    while let Some(message) = receiver.recv().await {
+        match message {
+            KeyValueMessage::Get(GetKeyValueMessage { key, response }) => {
+                let _ = response.send(map.get(&key).cloned());
+            }
+            KeyValueMessage::Delete(DeleteKeyValueMessage { key, response }) => {
+                map.remove(&key);
+                let _ = response.send(());
+            }
+            KeyValueMessage::Set(SetKeyValueMessage {
+                key,
+                value,
+                response,
+            }) => {
+                map.insert(key, value);
+                let _ = response.send(());
+            }
         }
     }
 }
 
-#[tokio::main]
-async fn main() {
-    let (tx, rx) = channel::<RespMessage>(100);
+async fn router(mut receiver: Receiver<RoutingMessage>) {
+    let (key_value_sender, key_value_receiver) = channel(32);
 
-    let _resp_actor_handle = tokio::spawn(resp_actor(rx));
+    tokio::spawn(key_value_actor(key_value_receiver));
 
-    for i in 0..10 {
-        let (resp_tx, resp_rx) = oneshot::channel::<i32>();
-        let msg = RespMessage {
-            value: i,
-            responder: resp_tx,
-        };
-
-        tx.send(msg).await.unwrap();
-        println!("Response: {}", resp_rx.await.unwrap());
+    while let Some(message) = receiver.recv().await {
+        match message {
+            RoutingMessage::KeyValue(message) => {
+                let _ = key_value_sender.send(message).await;
+            }
+        }
     }
+}
+
+async fn set(key: String, value: Vec<u8>) -> Result<(), std::io::Error> {
+    let (tx, rx) = oneshot::channel();
+    ROUTER_SENDER
+        .get()
+        .unwrap()
+        .send(RoutingMessage::KeyValue(KeyValueMessage::Set(
+            SetKeyValueMessage {
+                key,
+                value,
+                response: tx,
+            },
+        )))
+        .await
+        .unwrap();
+    rx.await.unwrap();
+    Ok(())
+}
+
+async fn get(key: String) -> Result<Option<Vec<u8>>, std::io::Error> {
+    let (tx, rx) = oneshot::channel();
+
+    ROUTER_SENDER
+        .get()
+        .unwrap()
+        .send(RoutingMessage::KeyValue(KeyValueMessage::Get(
+            GetKeyValueMessage { key, response: tx },
+        )))
+        .await
+        .unwrap();
+    Ok(rx.await.unwrap())
+}
+
+async fn delete(key: String) -> Result<(), std::io::Error> {
+    let (tx, rx) = oneshot::channel();
+
+    ROUTER_SENDER
+        .get()
+        .unwrap()
+        .send(RoutingMessage::KeyValue(KeyValueMessage::Delete(
+            DeleteKeyValueMessage { key, response: tx },
+        )))
+        .await
+        .unwrap();
+    rx.await.unwrap();
+    Ok(())
+}
+
+#[tokio::main]
+async fn main() -> Result<(), std::io::Error> {
+    let (sender, receiver) = channel(32);
+    ROUTER_SENDER.set(sender).unwrap();
+
+    tokio::spawn(router(receiver));
+
+    let _ = set("hello".to_string(), b"world".to_vec()).await?;
+
+    let value = get("hello".to_string()).await?;
+
+    println!("value: {:?}", String::from_utf8(value.unwrap()));
+
+    let _ = delete("hello".to_string()).await?;
+
+    let value = get("hello".to_string()).await?;
+
+    println!("value: {:?}", value);
+
+    Ok(())
 }
